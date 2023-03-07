b6cc680c698cf31bd2679b1c2e8a9f172be522f3
Ryan Jackson <rmjackson234@gmail.com>
2023-03-06 19:42:07 -0700

    moved .git up a directory

---
+++ mission09_rjackso3/Components/CategoriesViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using mission09_rjackso3.Models;
+
+namespace mission09_rjackso3.Components
+{
+	public class CategoriesViewComponent : ViewComponent
+	{
+		//creates a new repo
+		private IBookRepository repo { get; set; }
+
+		public CategoriesViewComponent(IBookRepository temp)
+		{
+			repo = temp;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			// Adds the filtered category to the viewbag so we can use it to highlight the button on the page
+            ViewBag.SelectedCategory = RouteData?.Values["Category"];
+
+			var categories = repo.Books
+				.Select(b => b.Category)
+				.Distinct()
+				.OrderBy(b => b);
+
+			return View(categories);
+        }
+	}
+}
+
---
+++ mission09_rjackso3/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mission09_rjackso3.Models;
+using mission09_rjackso3.Models.ViewModels;
+//using mission09_rjackso3.Models;
+
+namespace mission09_rjackso3.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public HomeController(IBookRepository temp) => repo = temp;
+        private IBookRepository repo;
+
+        public IActionResult Index(string category, int pageNum = 1)
+        {
+            int pageSize = 10;
+            var data = new BooksViewModel
+            {
+                Books = repo.Books
+                .Where(b => b.Category == category || category == null)
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+                PageInfo = new PageInfo
+                {
+                    TotalBookNum = (category == null ? repo.Books.Count() : repo.Books.Where(b=> b.Category == category).Count()),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(data);
+        }
+
+    }
+}
+
